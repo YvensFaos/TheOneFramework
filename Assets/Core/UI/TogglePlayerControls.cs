a5dee28b9230d0d4df83e58fa4d85dc7c2aa4d4d
acdh <a.c.denhond@saxion.nl>
2024-10-03 12:44:40 +0200

    Fix disable_controls, enable_controls
    A bugfix to repair enable/disable control script. Thanks to Lapra for reporting!

--- Assets/Core/UI/TogglePlayerControls.cs
+++ Assets/Core/UI/TogglePlayerControls.cs
@@ -9,7 +9,7 @@ public class TogglePlayerControls : MonoBehaviour
     public void SetInputState(bool enabled)
     {
         GetComponent<CharacterController>().enabled = enabled;
-        GetComponent<ThirdPersonController>().enabled = enabled;
+        GetComponent<ThirdPersonControllerFixed>().enabled = enabled;
         GetComponent<Animator>().enabled = enabled;
     }
     
